f17e95a37263d262d987f5ff599e8046401487ac
Samuel Leong <leowhiz@yahoo.com.sg>
2020-12-23 21:25:20 +0800

    Formatting + renmaing

--- ImageAlignment.cpp
+++ ImageAlignment.cpp
@@ -2,15 +2,46 @@
 
 ImageAlignment::ImageAlignment(/* args */) {}
 
-ImageAlignment::~ImageAlignment() {}
-
 // https://docs.opencv.org/master/da/d54/group__imgproc__transform.html#ga0203d9ee5fcd28d40dbc4a1ea4451983
 
+/**
+ * @brief Get BBOX (top, left, bottom, right)
+ *
+ * @return bbox_t current BBOX
+ */
 bbox_t &ImageAlignment::getBBOX() {
     return mBbox;
 }
 
+/**
+ * @brief Set BBOX (top, left, bottom, right)
+ *
+ * @param[in] aBbox BBOX
+ */
 void ImageAlignment::setBBOX(bbox_t aBbox) {
     for (int i = 0; i < 4; i++)
         mBbox[i] = aBbox[i];
+}
+
+/**
+ * @brief Set BBOX (top, left, bottom, right)
+ *
+ * @param[in] aTop Top of BBOX 
+ * @param[in] aLeft Left of BBOX
+ * @param[in] aBottom Bottom of BBOX
+ * @param[in] aRight Right of BBOX
+ */
+void ImageAlignment::setBBOX(float aTop, float aLeft, float aBottom,
+                             float aRight) {
+    mBbox[0] = aTop;
+    mBbox[1] = aLeft; 
+    mBbox[2] = aBottom;
+    mBbox[3] = aRight;
+}
+
+/**
+ * @brief Get template image
+ */
+cv::Mat &ImageAlignment::getTemplateImage() {
+    return mTemplateImage;
 }
--- ImageAlignment.hpp
+++ ImageAlignment.hpp
@@ -10,17 +10,25 @@
  *
  * Uses Baker-Matthews Inverse Compositional tracking algorithm
  * along with robust M-estimator to handle illumination differences
- * 
+ *
  * Handles the initialisation of a tracker with appropriate (sub-pixel) BBOX
  * Parent program calls `track()` to track the template image in the next frame
  * Updating the BBOX accordingly
  */
 
+/// @brief BBOX Type: Simple TLBR array
 typedef float bbox_t[4];
 
 class ImageAlignment {
   private:
+    /// @brief BBOX of template image (top, left, bottom, right)
     bbox_t mBbox;
+
+    /// @brief Template Image (previous frame)
+    cv::Mat &mTemplateImage;
+
+    /// @brief Current Image (current frame)
+    cv::Mat &mCurrentImage;
 
   public:
     KLTTracker(/* args */);
@@ -29,9 +37,14 @@ class ImageAlignment {
     // BBOX Interface
     bbox_t &getBBOX();
     void setBBOX(bbox_t aBbox);
+    void setBBOX(float aTop, float aLeft, float aBottom, float aRight);
+
+    // Get and Set Images
+    cv::Mat &getTemplateImage();
+    cv::Mat &getCurrImage();
 
     // Track
-    void track();
+    void track(cv::Mat &newImage);
 };
 
 #endif
