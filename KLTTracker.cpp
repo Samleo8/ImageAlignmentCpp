771cb1f8746226320f2c4e8e8c34380fa6e250cd
Samuel Leong <leowhiz@yahoo.com.sg>
2020-12-23 11:09:45 +0800

    Update basic KLT tracker template

--- KLTTracker.cpp
+++ KLTTracker.cpp
@@ -3,3 +3,5 @@
 KLTTracker::KLTTracker(/* args */) {}
 
 KLTTracker::~KLTTracker() {}
+
+// https://docs.opencv.org/master/da/d54/group__imgproc__transform.html#ga0203d9ee5fcd28d40dbc4a1ea4451983
--- KLTTracker.hpp
+++ KLTTracker.hpp
@@ -3,11 +3,23 @@
 
 #include <Eigen/Dense>
 #include <opencv2/opencv.hpp>
+#include <vector>
 
+/**
+ * @brief KLT Tracker Class
+ *
+ * Uses Baker-Matthews Inverse Compositional tracking algorithm
+ * along with robust M-estimator to handle illumination differences
+ * 
+ * Handles the initialisation of a tracker with appropriate (sub-pixel) BBOX
+ * Parent program calls `track()` to track the template image in the next frame
+ * Updating the BBOX accordingly
+ */
 class KLTTracker {
   private:
-    /* data */
+    /* private data */
   public:
+    float bbox[4];
     KLTTracker(/* args */);
     ~KLTTracker();
 };
