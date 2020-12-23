76da63ab5e7fd67ef7cba914bbb39a566c485ce9
Samuel Leong <leowhiz@yahoo.com.sg>
2020-12-23 18:03:19 +0800

    Fix issue with BBOX

--- KLTTracker.cpp
+++ KLTTracker.cpp
@@ -6,10 +6,11 @@ KLTTracker::~KLTTracker() {}
 
 // https://docs.opencv.org/master/da/d54/group__imgproc__transform.html#ga0203d9ee5fcd28d40dbc4a1ea4451983
 
-bbox_t &KLTTracker::getBBOX(){
-    return bbox;
+bbox_t &KLTTracker::getBBOX() {
+    return mBbox;
 }
 
 void KLTTracker::setBBOX(bbox_t aBbox) {
-    mBbox = aBbox;
+    for (int i = 0; i < 4; i++)
+        mBbox[i] = aBbox[i];
 }
